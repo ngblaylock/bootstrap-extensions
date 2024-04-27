037e327f08a7ed836dd74e0369b91936c24c2058
Nathan Blaylock <ngblaylock@gmail.com>
2024-04-27 17:05:26 -0600

    Move files around again

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({"dist/css/*.min.css": 'css'});
+	eleventyConfig.addPassthroughCopy({"11ty-site/_assets": '/'});
 	eleventyConfig.addPlugin(syntaxHighlight);
 	eleventyConfig.addFilter("sortByTitle", function(collection) {
     return collection.sort((a, b) => (a.data.title > b.data.title) ? 1 : -1);
@@ -9,7 +10,7 @@ module.exports = function (eleventyConfig) {
 	return {
 		dir: {
 			pathPrefix: 'bootstrap-extensions',
-      input: "pages",
+      input: "11ty-site",
 			includes: "_includes",
       layouts: "_layouts",
       output: "docs",
